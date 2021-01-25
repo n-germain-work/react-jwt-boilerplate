3e9b6d4075a076e055cbf0c1b3853f194a46db5b
bastienwcs <bastien.krettly@wildcodeschool.com>
2021-01-25 17:12:42 +0100

    propTypes

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,17 +1,27 @@
-/* eslint-disable react/prop-types */
-/* eslint-disable no-shadow */
 /* eslint-disable react/jsx-props-no-spreading */
 import React, { useContext } from "react";
+import PropTypes from "prop-types";
 import { Route, Redirect } from "react-router-dom";
 import { TokenContext } from "../contexts/TokenContext";
 
-export default function PrivateRoute(props) {
+const PrivateRoute = (props) => {
   const { token } = useContext(TokenContext);
   const { component: Component, ...rest } = props;
 
   if (token) {
-    return <Route {...rest} render={(props) => <Component {...props} />} />;
+    return (
+      <Route {...rest} render={(routeProps) => <Component {...routeProps} />} />
+    );
   }
   // redirect if there is no user
   return <Redirect to="/login" />;
-}
+};
+
+PrivateRoute.propTypes = {
+  component: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node,
+  ]).isRequired,
+};
+
+export default PrivateRoute;
